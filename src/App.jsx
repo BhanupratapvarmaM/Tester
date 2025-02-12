ef8711d18ceba4fbd019ae3e7ed87fc6f7f49595
BhanupratapvarmaM <119054769+BhanupratapvarmaM@users.noreply.github.com>
2025-02-12 13:24:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css"; // Import the CSS for styling
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
+
 import Home from "./Pages/Home";
 import Contact from "./Pages/Contact";
 import Registrations from "./Pages/Registrations";
